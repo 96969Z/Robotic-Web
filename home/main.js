f16dd516cb2d2dbfd150be8f2793a23706153314
96969Z <88474801+96969Z@users.noreply.github.com>
2021-09-14 15:49:11 +0800

    Add files via upload

---
+++ home/main.js
@@ -0,0 +1,72 @@
+// ----------------------------------------------------------toggle---------------------------------------------------//
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+} 
+
+
+
+// ----------------------------------------------------------image animation---------------------------------------------------//
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+
+
+// ----------------------------------------------------------popup box---------------------------------------------------//
+// const card = document.querySelector(".board")
+// const popup = document.querySelector(".popup-box")
+
+// card.addEventListener("click",function(event){
+//   if(event.target.tagName.toLowerCase() == "button"){
+//     const item = event.target.parentElement;
+//     const h3 = item.querySelector("h3").innerHTML;
+//     const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+
+//     popup.querySelector("h3").innerHTML = h3;
+//     popup.querySelector(".popup-body").innerHTML = readMoreCont;
+//     popupBox()  }
+// })
+
+// function popupBox(){
+//   popup.classList.toggle("open")
+// }
